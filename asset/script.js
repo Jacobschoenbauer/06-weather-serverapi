1d2533ed833f4caa45302ebfa48b10d9138655e1
Jacobschoenbauer <jhwt65@gmail.com>
2022-10-06 11:50:46 -0500

    got it to work

--- asset/script.js
+++ asset/script.js
@@ -1,21 +1,17 @@
 const APIkey = "f4cba9e7f452234c91b154152aa8da72";
 
-var repoList = document.querySelector("ul");
-var fetchButton = document.getElementById("search");
-
+const fetchButton = document.getElementById("search");
+const lat = 44.71469;
+const lon = -93.261124;
 function getApi() {
-  var requestUrl =
-    "https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`";
+  const requestUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=${APIkey}&lat=${lat}&lon=${lon}`;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-        console.log(data)
-    })
-      
-      
+      console.log(data);
+    });
 }
 
-
-fetchButton.addEventListener("button", getApi);
+fetchButton.addEventListener("click", getApi);
