4ddbb9c553682de38f75cbd6846a337aa10d062d
Jacobschoenbauer <jhwt65@gmail.com>
2022-10-08 05:50:00 -0500

    tried and earsed getting content to post

--- asset/script.js
+++ asset/script.js
@@ -1,10 +1,14 @@
 const APIkey = "f4cba9e7f452234c91b154152aa8da72";
-let repoList = document.querySelector("ul");
+let tempDegree = document.querySelector(".temp");
+let locateTime = document.querySelector(".location");
+let tempData = document.querySelector(".description");
+
 const fetchButton = document.getElementById("search");
 let cityName = document.getElementById("searchcity").value;
-
+let lat = lat
+let lon = lon
 function getApi() {
-  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=lakeville&appid=${APIkey}`;
+  let requestUrl = `https://api.openweathermap.org/data/2.5/uvi?q=lakeville&appid=${APIkey}&lat=${lat}&lon=${lon}`;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -12,9 +16,7 @@ function getApi() {
     .then(function (data) {
       console.log(data);
 
-      var weatherPost = document.createElement("h3");
-
-      weatherPost.textContent = data.list[1].title;
+      
 
      
     });
@@ -23,3 +25,4 @@ function getApi() {
 fetchButton.addEventListener("click", getApi);
 
 //https://api.openweathermap.org/data/2.5/uvi?appid=${APIkey}&lat=${lat}&lon=${lon}
+//https://api.openweathermap.org/data/2.5/forecast?q=lakeville&appid=${APIkey}
