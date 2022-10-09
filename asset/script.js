7a34a64ba15040e9ef7ab58a1b6e1c6aeda3a7fe
Jacobschoenbauer <jhwt65@gmail.com>
2022-10-09 14:56:22 -0500

    moved url into get api to make work

--- asset/script.js
+++ asset/script.js
@@ -1,44 +1,46 @@
 const APIkey = "f4cba9e7f452234c91b154152aa8da72";
-let citySearch = "Lakeville"
-let cityNames = ""
+let cityNames = "";
 const fetchButton = document.getElementById("search");
-let cityName = document.getElementById("searchcity");
-
-
-let requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ citySearch +"&appid=" + APIkey+"&units=imperial"
+let cityInput = document.getElementById("searchcity");
+const placeTemp = document.querySelector(".temp");
+const placeHumidty = document.querySelector(".humidity");
+const placeWind = document.querySelector(".windspeed");
 
 function getApi() {
-  
+  let requestUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityNames +
+    "&appid=" +
+    APIkey +
+    "&units=imperial";
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (tempData) {
-     console.log(tempData)
-     let currentTemp = tempData.main.temp
-     console.log(currentTemp)
-      let placeTemp = document.querySelector(".temp")
-      placeTemp.textContent=currentTemp
+      console.log(tempData);
 
-      let currentHumidty = tempData.main.humidity
-      console.log(currentHumidty)
-      let placeHumidty = document.querySelector(".humidity")
-      placeHumidty.textContent=currentHumidty
+      let currentTemp = tempData.main.temp;
+      console.log(currentTemp);
+      placeTemp.textContent = currentTemp + " degrees";
 
-      let currentWind = tempData.wind.speed
-      console.log(currentWind)
-      let placeWind = document.querySelector(".windspeed")
-      placeWind.textContent=currentWind
-     
+      let currentHumidty = tempData.main.humidity;
+      console.log(currentHumidty);
+      placeHumidty.textContent = currentHumidty + " %";
+
+      let currentWind = tempData.wind.speed;
+      console.log(currentWind);
+      placeWind.textContent = currentWind + " mph";
     });
 }
 
-fetchButton.addEventListener("click", function(){
+fetchButton.addEventListener("click", function () {
+  cityNames = cityInput.value;
+  console.log(cityNames);
+  let placeName = document.querySelector(".thename");
+  placeName.textContent = cityNames;
 
-  cityNames = cityName.value
-  console.log(cityNames)
-  getApi()
-
+  getApi();
 });
 
 //https://api.openweathermap.org/data/2.5/uvi?appid=${APIkey}&lat=${lat}&lon=${lon}
